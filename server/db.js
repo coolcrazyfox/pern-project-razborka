352b604485d6c300bf466c366a6d83f4f9573696
Andrei Malets <coolviktori@yandex.by>
2022-10-29 17:21:07 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "man21061985xxx",
+  password: "man",
   host: "localhost",
   port: 5432,
   database: "mybase"
